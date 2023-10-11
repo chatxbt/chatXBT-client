6457783c0d2ade63cfe8ee0f9eb2da0b0f7c73cb
LONGYKING <39104274+LONGYKING@users.noreply.github.com>
2023-10-11 04:18:56 +0100

    updates

--- chatxbt-sdk/utils/nlp-core/intent-handler/index.ts
+++ chatxbt-sdk/utils/nlp-core/intent-handler/index.ts
@@ -31,126 +31,150 @@ export class IntentHandler {
   }
 
   async buyTokenWithEth(to: 'usdt', amountIn: string, dex: 'uniswap', p: string) {
-    // alert(`to: ${to} \n amountIn: ${amountIn} \n dex: ${dex} \n provider: ${provider}`);
-    let signer = null;
-    let address = ""
-    let tx;
-    if (p.toLowerCase() === 'metamask') {
-      // alert(`to: ${to} \n amountIn: ${amountIn} \n dex: ${dex} \n provider: ${p}`);
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      address = accounts[0];
-      signer = provider.getSigner()
-    }
-    if (signer) {
-      let toToken;
-      let router;
-      if (to.startsWith("0x")) { // it's resolved already
-        toToken = to;
-      } else {
-        toToken = tokens[to]
+    try {
+      // alert(`to: ${to} \n amountIn: ${amountIn} \n dex: ${dex} \n provider: ${provider}`);
+      let signer = null;
+      let address = ""
+      let tx;
+      if (p.toLowerCase() === 'metamask') {
+        // alert(`to: ${to} \n amountIn: ${amountIn} \n dex: ${dex} \n provider: ${p}`);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        address = accounts[0];
+        signer = provider.getSigner()
       }
+      if (signer) {
+        let toToken;
+        let router;
+        if (to.startsWith("0x")) { // it's resolved already
+          toToken = to;
+        } else {
+          toToken = tokens[to]
+        }
 
-      if(dex.startsWith("0x")) {
-        router = dex;
-      } else {
-        router = routers[dex]
+        if(dex.startsWith("0x")) {
+          router = dex;
+        } else {
+          router = routers[dex]
+        }
+
+        const path = [tokens['weth'], toToken];
+        const contract = toolkit.makeContract(router, routerV2ABI, signer)
+        const amountsOut = await contract.getAmountsOut(ethers.utils.parseEther(String(amountIn)), path);
+        const now = new Date()
+        tx = await contract.swapExactETHForTokensSupportingFeeOnTransferTokens(
+          amountsOut[1],
+          path,
+          address,
+          new Date(now.setMinutes(now.getMinutes() + 5)).getTime(), { value: ethers.utils.parseEther(String(amountIn)) }
+        )
+        await tx.wait();
       }
-      console.log('r',{ router })
-      const path = [tokens['weth'], toToken];
-      const contract = toolkit.makeContract(router, routerV2ABI, signer)
-      const amountsOut = await contract.getAmountsOut(ethers.utils.parseEther(String(amountIn)), path);
-      const now = new Date()
-      tx = await contract.swapExactETHForTokensSupportingFeeOnTransferTokens(
-        amountsOut[1],
-        path,
-        address,
-        new Date(now.setMinutes(now.getMinutes() + 5)).getTime(), { value: ethers.utils.parseEther(String(amountIn)) }
-      )
-      await tx.wait();
+      return { 
+        status: true,
+        type: 'swap', 
+        message: tx.hash, 
+        metadata: {
+          ...tx
+        }
+      };
+    } catch (error: any) {
+      return { 
+        status: true,
+        type: 'default-text',
+        message: `Unable to swap: ${error?.message}`
+      };
     }
-    return { 
-      status: true,
-      type: 'swap', 
-      message: tx.hash, 
-      metadata: {
-        ...tx
-      }
-    };
   }
   async sellTokenForEth(from: 'usdt', amountIn: string, dex: 'uniswap', wallertProvider: string) {
-    const router = routers[dex]
-    let signer = null;
-    let address = ""
-    let tx;
-    if (wallertProvider.toLowerCase() === 'metamask') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      address = accounts[0];
-      signer = provider.getSigner()
-    }
-    if (signer) {
-      const path = [tokens[from], tokens['weth']];
-      // alert(path);
-      const contract = toolkit.makeContract(router, routerV2ABI, signer)
-      const amountsIn = await contract.connect(signer).getAmountsOut(ethers.utils.parseEther(String(amountIn)), path);
-      const now = new Date()
-      tx = await contract.swapExactTokensForETHSupportingFeeOnTransferTokens(
-        amountsIn[0],
-        0,
-        path,
-        address,
-        new Date(now.setMinutes(now.getMinutes() + 5)).getTime()
-      )
-      await tx.wait();
-    }
-    return { 
-      status: true,
-      type: 'swap', 
-      message: tx.hash,
-      metadata: {
-        ...tx
+    try {
+      const router = routers[dex]
+      let signer = null;
+      let address = ""
+      let tx;
+      if (wallertProvider.toLowerCase() === 'metamask') {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        address = accounts[0];
+        signer = provider.getSigner()
       }
-    };
+      if (signer) {
+        const path = [tokens[from], tokens['weth']];
+        // alert(path);
+        const contract = toolkit.makeContract(router, routerV2ABI, signer)
+        const amountsIn = await contract.connect(signer).getAmountsOut(ethers.utils.parseEther(String(amountIn)), path);
+        const now = new Date()
+        tx = await contract.swapExactTokensForETHSupportingFeeOnTransferTokens(
+          amountsIn[0],
+          0,
+          path,
+          address,
+          new Date(now.setMinutes(now.getMinutes() + 5)).getTime()
+        )
+        await tx.wait();
+      }
+      return { 
+        status: true,
+        type: 'swap', 
+        message: tx.hash,
+        metadata: {
+          ...tx
+        }
+      };
+    } catch (error: any) {
+      return { 
+        status: true,
+        type: 'default-text',
+        message: `Unable to swap: ${error?.message}`
+      };
+    }
   }
 
   async giveTokenSpendApproval(account: string, token: 'usdt', provider: string) {
-    let signer = null;
-    let address = ""
-    let value = "0";
-    let tx;
-    if (provider === 'metamask') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      address = accounts[0];
-      signer = provider.getSigner()
-    }
-    if (signer) {
-      const contract = toolkit.makeContract(token, tokenABI, signer)
-      const allowance = await contract.allowance(address, account);
-
-      const decimals = await contract.decimals();
-      value = Number(ethers.utils.formatUnits(allowance, decimals)).toFixed(4);
-      // value = Number(ethers.utils.formatUnits(allowance, decimals)).toFixed(4);
-      if (value === "0") {
-        tx = await contract.approve(account, ethers.constants.MaxUint256)
-        await tx.wait();
+    try {
+      let signer = null;
+      let address = ""
+      let value = "0";
+      let tx;
+      if (provider === 'metamask') {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        address = accounts[0];
+        signer = provider.getSigner()
       }
-    }
-    return { 
-      type: 'approval', 
-      message: value,
-      status: true,
-      metadata: {
-        ...tx
+      if (signer) {
+        const contract = toolkit.makeContract(token, tokenABI, signer)
+        const allowance = await contract.allowance(address, account);
+  
+        const decimals = await contract.decimals();
+        value = Number(ethers.utils.formatUnits(allowance, decimals)).toFixed(4);
+        // value = Number(ethers.utils.formatUnits(allowance, decimals)).toFixed(4);
+        if (value === "0") {
+          tx = await contract.approve(account, ethers.constants.MaxUint256)
+          await tx.wait();
+        }
       }
-    };
+      return { 
+        type: 'approval', 
+        message: value,
+        status: true,
+        metadata: {
+          ...tx
+        }
+      };
+    } catch (error: any) {
+      return { 
+        status: true,
+        type: 'default-text',
+        message: `Unable to swap: ${error?.message}`
+      };
+    }
   }
 
   async getCoinPrice({ coin, to = 'usd', dex, amount = 1 }: { coin: string, to?: string, dex: string, amount?: number }) {
