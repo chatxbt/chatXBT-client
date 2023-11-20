c2d2a79a3dc3cc8bb6faeae816c35153154d6b29
Somtochukwu Nkemakolam <sewkito@gmail.com>
2023-11-20 18:50:17 +0100

    no changes

--- chatxbt-sdk/hooks/appEntry/index.ts
+++ chatxbt-sdk/hooks/appEntry/index.ts
@@ -1,81 +1,62 @@
-import { handleRefs } from "@chatxbt-sdk/utils";
+import { handleRefs } from "../../utils";
 import { useEffect } from "react";
-import {
-    chatxbtServices
-} from "../../../chatxbt-sdk"
+import { chatxbtServices } from "../../../chatxbt-sdk";
 
 export const useAppEntry = (props: any) => {
-    try {
-        const authService = chatxbtServices.auth(props);
-        const defiService = chatxbtServices.defi(props);
-        const {
-            store: { 
-                _hasHydrated,
-                wagmiData,
-                variables,
-                signMessageData,
-                connected
-            },
-            action: { 
-                handleWalletSignIn,
-                signAndConnectUser,
-                signOut
-            }
-        } = authService;
-    
-        const {
-            store: { 
-                configured,
-                lightPool
-            },
-            action: { 
-                loadLightPoolAndInitialiseNlpCoreConfigs
-            }
-        } = defiService
-    
-        useEffect(() => {
-            !connected
-            && _hasHydrated
-            && wagmiData.isConnected 
-            && wagmiData.address 
-            && handleWalletSignIn(wagmiData.address);
-        }, [wagmiData.isConnected, wagmiData.address]);
-    
-        useEffect(() => {
-            // localStorage.clear();
-            // connected && props.history.push('/chat');
-            // !connected && props.history.push('/');
-            // signOut();
-            !configured && connected && loadLightPoolAndInitialiseNlpCoreConfigs()
-        }, [connected]);
+  try {
+    const authService = chatxbtServices.auth(props);
+    const defiService = chatxbtServices.defi(props);
+    const {
+      store: { _hasHydrated, wagmiData, variables, signMessageData, connected },
+      action: { handleWalletSignIn, signAndConnectUser, signOut },
+    } = authService;
 
-        useEffect(() => {
-            // localStorage.clear();
-            wagmiData?.isDisconnected && signOut();
-        }, [wagmiData?.isDisconnected]);
+    const {
+      store: { configured, lightPool },
+      action: { loadLightPoolAndInitialiseNlpCoreConfigs },
+    } = defiService;
 
-        useEffect(() => {
-            ;(async () => {
-              if (variables?.message && signMessageData) {
-                // const recoveredAddress = await recoverMessageAddress({
-                //   message: variables?.message,
-                //   signature: signMessageData,
-                // })
-                // setRecoveredAddress(recoveredAddress)
-                await signAndConnectUser(wagmiData.address)
-              }
-            })()
-        }, [signMessageData, variables?.message])
-    
-        return {
-            store: {
-                connected
-            },
-            action: {
-                loadLightPoolAndInitialiseNlpCoreConfigs
-            }
-        };
-    } catch (error) {
-        
-    }
-}
+    useEffect(() => {
+      !connected &&
+        _hasHydrated &&
+        wagmiData.isConnected &&
+        wagmiData.address &&
+        handleWalletSignIn(wagmiData.address);
+    }, [wagmiData.isConnected, wagmiData.address]);
+
+    useEffect(() => {
+      // localStorage.clear();
+      // connected && props.history.push('/chat');
+      // !connected && props.history.push('/');
+      // signOut();
+      !configured && connected && loadLightPoolAndInitialiseNlpCoreConfigs();
+    }, [connected]);
+
+    useEffect(() => {
+      // localStorage.clear();
+      wagmiData?.isDisconnected && signOut();
+    }, [wagmiData?.isDisconnected]);
+
+    useEffect(() => {
+      (async () => {
+        if (variables?.message && signMessageData) {
+          // const recoveredAddress = await recoverMessageAddress({
+          //   message: variables?.message,
+          //   signature: signMessageData,
+          // })
+          // setRecoveredAddress(recoveredAddress)
+          await signAndConnectUser(wagmiData.address);
+        }
+      })();
+    }, [signMessageData, variables?.message]);
+
+    return {
+      store: {
+        connected,
+      },
+      action: {
+        loadLightPoolAndInitialiseNlpCoreConfigs,
+      },
+    };
+  } catch (error) {}
+};
