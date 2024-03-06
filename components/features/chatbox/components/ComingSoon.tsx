3a7fb066bec48894e1f1ee13bde72f76c400c07b
S0mt0 <sewkito@gmail.com>
2024-03-06 22:52:46 +0100

    testing and debugging features

--- components/features/chatbox/components/ComingSoon.tsx
+++ components/features/chatbox/components/ComingSoon.tsx
@@ -6,9 +6,15 @@ const ComingSoon = () => {
     <div className={style.divCon}>
       <h1>Activity</h1>
       <p>
+        Ready to take action? Explore our range of DeFi services and features,
+        including swapping, borrowing, wallet management, and more. Begin your
+        DeFi journey today and unlock the full potential of decentralized
+        finance.
+      </p>
+      {/* <p>
         Gain insights into your DeFi journey and monitor the status of your
         transactions in real-time.
-      </p>
+      </p> */}
     </div>
   );
 };
