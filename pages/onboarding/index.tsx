f9a3152f57187566ad80688dce5f6849a94b3ee3
LouisSeb16 <sebastineokey@gmail.com>
2024-03-11 15:58:12 +0100

    Cleanup

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -3,25 +3,29 @@ import dynamic from "next/dynamic";
 import ChatPage from "../dashboard/chat";
 
 const DynamicPage = dynamic(
-    async () => await import("@components/features/ai-onboarding"),
-    { ssr: false }
-  );
+  async () => await import("@components/features/ai-onboarding"),
+  { ssr: false }
+);
 
 const OnboardingPage = (props: any) => {
   const {
-    store: { connected, userInfo},
+    store: { connected, userInfo },
   } = props;
 
   const [isOnboarded, setOnboarded] = useState(false);
   const setOnboard = (state: boolean) => {
     setOnboarded(state);
-  }
+  };
 
   return (
     <>
-      {userInfo?.displayName || isOnboarded ? <ChatPage /> : <DynamicPage setOnboard={setOnboard} />}
+      {userInfo?.displayName || isOnboarded ? (
+        <ChatPage />
+      ) : (
+        <DynamicPage setOnboard={setOnboard} />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default OnboardingPage
+export default OnboardingPage;
