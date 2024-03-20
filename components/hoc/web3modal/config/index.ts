3c2bd807698a1bdab04bff9ccd664a587da7747c
chidi longinus <icodeidea@chidis-MacBook-Pro.local>
2024-03-20 07:42:24 +0100

    forked mainnet

--- components/hoc/web3modal/config/index.ts
+++ components/hoc/web3modal/config/index.ts
@@ -42,6 +42,7 @@ import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { cookieStorage, createStorage } from 'wagmi'
 import { mainnet, sepolia, goerli } from 'wagmi/chains'
+import { defineChain } from 'viem'
 
 // Get projectId at https://cloud.walletconnect.com
 // export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || '954d1cd106b485e394a1b5b7423a42bd'
@@ -56,15 +57,30 @@ const metadata = {
   icons: ['https://www.chatxbt.com/images/logo/logo-2.png']
 }
 
-export const chatxbtchain = {
-  ...mainnet,
-  chainId: 15779,
-  name: 'Chatxbt',
-  currency: 'ETH',
-  explorerUrl: 'https://explorer.buildbear.io/zany-gilgamesh-cf3ecd70',
-  rpcUrl: 'https://rpc.buildbear.io/zany-gilgamesh-cf3ecd70',
-
-}
+export const chatxbtchain = defineChain({
+  id: 15779,
+  name: 'chatxbt',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc.buildbear.io/zany-gilgamesh-cf3ecd70'] },
+  },
+  blockExplorers: {
+    default: { name: 'chatxbt', url: 'https://explorer.buildbear.io/zany-gilgamesh-cf3ecd70' },
+  },
+  contracts: {
+    ensRegistry: {
+      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+    },
+    ensUniversalResolver: {
+      address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da',
+      blockCreated: 16773775,
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 14353601,
+    },
+  },
+})
 
 // Create wagmiConfig
 export const config = defaultWagmiConfig({
