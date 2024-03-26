079a7edceb3c91536ff58e449c2c27b92dab73af
LouisSeb16 <sebastineokey@gmail.com>
2024-03-26 17:53:42 +0100

    UI fixes

--- components/features/tokenomics/Overview.tsx
+++ components/features/tokenomics/Overview.tsx
@@ -9,28 +9,33 @@ const Overview = () => {
   return (
     <>
       {openModal && <ReferralModal handleTaskModal={handleTaskModal} />}
-      
-        <div className={`container ${style.overview}`}>
-      <div className="row">
-        <div className="col-md-8">
-          <div className={style.card}>
-            <h3>ChatXBT Points</h3>
-            <h1>
-              0 <span>points</span>
-            </h1>
+
+      <div className={`container ${style.overview}`}>
+        <div className="row">
+          <div className="col-md-8">
+            <div className={style.card}>
+              <h3>ChatXBT Points</h3>
+              <h1>
+                0 <span>points</span>
+              </h1>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className={style.card}>
+              <h3>Referrals</h3>
+              <h1>0</h1>
+            </div>
           </div>
         </div>
-        <div className="col-md-4">
-          <div className={style.card}>
-            <h3>Referrals</h3>
-            <h1>0</h1>
-          </div>
+        <div className={style.share}>
+          <button onClick={handleTaskModal} id={style.x}>
+            Connect X
+          </button>
+          <button onClick={handleTaskModal}>
+            Share Referral <BsArrowUpRight />
+          </button>
         </div>
       </div>
-      <div className={style.share}>
-        <button onClick={handleTaskModal}>Share Referral <BsArrowUpRight /></button>
-      </div>
-    </div>
     </>
   );
 };
