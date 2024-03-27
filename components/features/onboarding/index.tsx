4e9cbf00445d76e7f9851d8e37698786c6d35ae2
LouisSeb16 <sebastineokey@gmail.com>
2024-03-27 20:28:34 +0100

    SEO fix

--- components/features/onboarding/index.tsx
+++ components/features/onboarding/index.tsx
@@ -3,10 +3,17 @@ import { classInit } from "@chatxbt-sdk/utils";
 import style from "@styles/get-started/index.module.scss";
 import GetStartedLayout from "@components/app-layout/layout/GetStartedLayout";
 import Main from "./Main";
+import AppSeo from "@components/shared/seo";
+import { seoImage } from "@chatxbt-sdk/utils/assets";
 
 const OnboardingComponent = (props: any) => {
   return (
     <>
+      <AppSeo
+        title="Unlock a new experience with the power of AI and DeFi."
+        description={`Trade, lend, stake, transfer, create multisigs, wallet, interact with protocols...Not with clicks, but with chat.`}
+        image={seoImage.default}
+      />
       <GetStartedLayout>
         <div className={classInit.addConClass(style.getStartedCon)}>
           <Main {...props} />
--- components/features/tokenomics/index.tsx
+++ components/features/tokenomics/index.tsx
@@ -4,10 +4,17 @@ import Overview from "./Overview";
 import GetStartedNavPoints from "@components/app-layout/nav/GetStartedNavPoints";
 import Tasks from "./Tasks";
 import Footer from "@components/app-layout/footer";
+import AppSeo from "@components/shared/seo";
+import { seoImage } from "@chatxbt-sdk/utils/assets";
 
 const TokenomicsComponent = () => {
   return (
     <>
+      <AppSeo
+        title="Unlock a new experience with the power of AI and DeFi."
+        description={`Experience Chatxbt: Your gateway to DeFi rewards! Earn points by completing tasks and referring friends. Join us today!`}
+        image={seoImage.default}
+      />
       <div className={`container-fluid ${style.tokenomicsPage}`}>
         <div id={style.neon}></div>
         <GetStartedNavPoints />
